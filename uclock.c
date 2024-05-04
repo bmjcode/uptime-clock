e42305470ba05945f70f1b752968709150cf99ff
Benjamin Johnson <42893476+bmjcode@users.noreply.github.com>
2024-05-04 12:18:00 -0400

    Update the copyright date

--- uclock.c
+++ uclock.c
@@ -1,6 +1,6 @@
 /*
  * Clock with system uptime display.
- * Copyright (c) 2023 Benjamin Johnson <bmjcode@gmail.com>
+ * Copyright (c) 2023, 2024 Benjamin Johnson <bmjcode@gmail.com>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
